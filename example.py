987c7afda3a68fdb9c356e01d68de5e12b4083de
AnhP <157474368+PhamHuynhAnh16@users.noreply.github.com>
2026-06-06 13:34:26 +0700

    Update example.py

--- example.py
+++ example.py
@@ -1,11 +1,14 @@
 import torch
 import librosa
 import onnxruntime
+
+import numpy as np
 import soundfile as sf
 
 input_model = "contentvec_base.onnx"
 input_audio = "test.wav"
 output_layer = 12
+final_proj = True
 
 def get_providers():
     ort_providers = onnxruntime.get_available_providers()
@@ -16,8 +19,50 @@ def get_providers():
 
     return providers
 
-def extract_features(model, feats, layer):
-    return torch.as_tensor(model.run(["feats_9", "feats_12"], {"feats": feats.detach().cpu().numpy()})[int(layer != 9)], dtype=torch.float32, device=feats.device)
+class HubertModelONNX:
+    def __init__(
+        self, 
+        embedder_model_path, 
+        providers
+    ):
+        sess_options = onnxruntime.SessionOptions()
+        sess_options.log_severity_level = 3
+
+        self.model = onnxruntime.InferenceSession(
+            embedder_model_path, 
+            sess_options=sess_options, 
+            providers=providers
+        )
+
+        self._finalproj = False
+        self.final_proj = self._final_proj
+
+    def _final_proj(self, source):
+        return source
+    
+    def extract_features(self, source, output_layer = None):
+        device = source.device
+
+        logits = self.model.run(
+            [self.model.get_outputs()[0].name, self.model.get_outputs()[1].name], 
+            {
+                self.model.get_inputs()[0].name: source.float().detach().cpu().numpy(),
+                self.model.get_inputs()[1].name: np.array(output_layer, dtype=np.int64),
+            }
+        )
+
+        return [
+            torch.as_tensor(
+                logits[int(self._finalproj)], 
+                dtype=torch.float32, 
+                device=device
+            )
+        ]
+
+hubert_model = HubertModelONNX(
+    input_model, 
+    get_providers()
+)
 
 def load_audio(file):
     audio, sr = sf.read(file)
@@ -25,16 +70,15 @@ def load_audio(file):
     if sr != 16000: audio = librosa.resample(audio, orig_sr=sr, target_sr=16000, res_type="soxr_vhq")
     return audio.flatten()
 
-sess_options = onnxruntime.SessionOptions()
-sess_options.log_severity_level = 3
-models = onnxruntime.InferenceSession(input_model, sess_options=sess_options, providers=get_providers())
 feats = torch.from_numpy(load_audio(input_audio)).float()
 
 if feats.dim() == 2: feats = feats.mean(-1)
 assert feats.dim() == 1, feats.dim()
-
 feats = feats.view(1, -1)
-feats = extract_features(models, feats, output_layer)
+
+hubert_model._finalproj = final_proj
+feats = hubert_model.extract_features(feats, output_layer)
+feats = feats[0]
 
 print("Shape:", feats.shape)
 print(feats)
